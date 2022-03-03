b25c2e2da6ee4ba242612a235d079242d2258c71
Charlotte Vermandel <charlottevermandel@gmail.com>
2022-03-03 17:08:47 +0100

    Add all required content types methods

--- meilisearch/server/services/content-types/content-types.js
+++ meilisearch/server/services/content-types/content-types.js
@@ -1,11 +1,4 @@
 'use strict'
-// TODO: fetch documents ✅
-// Bulk action with fetched documents ✅
-// TODO: Use filtering configuration for entry fetching
-// TODO: listCollectionsWithIndexName -> Return all collections having the provided indexName setting. ✅
-// TODO: numberOfEntries/totalNumberOfEntries -> Number of entries in a collection.  ✅
-// TODO: allEligbleCollections (not only multi type collections) ❌ Temp: getApisName
-// TODO: getEntriesBatch ✅
 
 module.exports = ({ strapi }) => ({
   /**
@@ -155,114 +148,112 @@ module.exports = ({ strapi }) => ({
   },
 
   /**
-   * Wether the collection exists or not
+   * Wether the content type exists or not.
    *
-   * @param  {string} collection - Name of the collection.
-   *
-   * @returns  {number}
-   */
-  collectionExists({ collection }) {
-    return !!this.getApisName().find(api => api === collection)
-  },
-
-  /**
-   * Return all collections having the provided indexName setting.
-   *
-   * @param  {string} indexName
-   */
-  listCollectionsWithIndexName: async function ({ indexName }) {
-    const multiRowsCollections = this.getApisName() || []
-    const collectionsWithIndexName = multiRowsCollections.filter(
-      collection =>
-        strapi
-          .plugin('meilisearch')
-          .service('contentTypes')
-          .getIndexName({ indexName: collection }) === indexName
-    )
-    return collectionsWithIndexName
-  },
-
-  /**
-   * Number of entries in a collection.
-   *
-   * @param  {string} collection - Name of the collection.
+   * @param  {string} contentType - Name of the content type.
    *
    * @returns  {number}
    */
-  numberOfEntries: async function ({ collection, where = {} }) {
-    if (!this.collectionExists({ collection })) return 0
+  contentTypeExists({ contentType }) {
+    return !!Object.keys(strapi.contentTypes).includes(contentType)
+  },
 
-    const count = await strapi.db
-      .query(`api::${collection}.${collection}`)
-      .count({ where })
+  /**
+   * Number of entries in a content type.
+   *
+   * @param  {string} contentType - Name of the contentType.
+   *
+   * @returns  {number} number of entries in the content type.
+   */
+  numberOfEntries: async function ({ contentType, where = {} }) {
+    if (!this.contentTypeExists({ contentType })) return 0
+
+    const count = await strapi.db.query(contentType).count({ where })
     return count
   },
 
   /**
-   * Returns the total number of entries of the collections.
+   * Returns the total number of entries of the content types.
    *
-   * @param  {string[]} collections
+   * @param  {string[]} contentTypes List of the content types.
    *
-   * @returns {number} Total entries number.
+   * @returns {number} Total entries number of the content types.
    */
-  totalNumberOfEntries: async function ({ collections }) {
-    let collectionsEntriesSize = await Promise.all(
-      collections.map(async col => await this.numberOfEntries(col))
+  totalNumberOfEntries: async function ({ contentTypes }) {
+    let numberOfEntries = await Promise.all(
+      contentTypes.map(async contentType =>
+        this.numberOfEntries({ contentType })
+      )
     )
-    return collectionsEntriesSize.reduce((acc, curr) => (acc += curr), 0)
+
+    const entriesSum = numberOfEntries.reduce((acc, curr) => (acc += curr), 0)
+
+    return entriesSum
   },
 
   /**
-   * Returns a batch of entries.
+   * Returns a batch of entries of a given content type.
+   * More information: https://docs.strapi.io/developer-docs/latest/developer-resources/database-apis-reference/entity-service/crud.html#findmany
    *
-   * @param  {object} batchOptions
-   * @param  {number} start - Starting batch number.
-   * @param  {number} limit - Size of batch.
-   * @param  {string} collection - Collection name.
+   * @param  {number} fields - Fields present in the returned entry.
+   * @param  {number} start - Pagination start.
+   * @param  {number} limit - Number of entries to return.
+   * @param  {object} filters - Filters to use.
+   * @param  {object} sort - Order definition.
+   * @param  {object} populate - Relations, components and dynamic zones to populate.
+   * @param  {object} publicationState - Publication state: live or preview.
+   * @param  {string} contentType - Content type.
    *
    * @returns  {object[]} - Entries.
    */
-  async findManyOfCollection({ collection, start = 0, limit = 500 }) {
-    if (!this.collectionExists({ collection })) return []
+  async getContentTypeEntries({
+    contentType,
+    fields = '*',
+    limit = 500,
+    filters = {},
+    sort = {},
+    populate = {},
+    publicationState,
+  }) {
+    if (!this.contentTypeExists({ contentType })) return []
 
-    // TODO: get filters from settings
-    // https://docs.strapi.io/developer-docs/latest/developer-resources/database-apis-reference/entity-service/crud.html#findmany
-    const entries = await strapi.entityService.findMany(
-      // `api::${apiName}.${contentType}`: uid
-      `api::${collection}.${collection}`, // TODO: only works with content types
-      {
-        publicationState: 'live',
-        start,
-        limit,
-      }
-    )
+    const entries = await strapi.entityService.findMany(contentType, {
+      fields: fields || '*',
+      limit,
+      filters,
+      sort,
+      populate,
+      publicationState: publicationState || 'live',
+    })
     return entries || []
   },
 
   /**
-   * Apply an action on all the entries of the provided collection.
+   * Apply an action on all the entries of the provided content type.
    *
-   * @param  {string} collection
-   * @param  {function} callback - Function applied on each entry of the collection
+   * @param  {string} contentType - Name of the content type.
+   * @param  {function} callback - Function applied on each entry of the contentType.
    *
    * @returns {any[]} - List of all the returned elements from the callback.
    */
-  actionInBatches: async function ({ collection, callback }) {
+  actionInBatches: async function ({ contentType, callback = () => {} }) {
     const BATCH_SIZE = 500
     // Need total number of entries in collection
-    const entries_count = await this.numberOfEntries({ collection })
-    const response = []
+    const entries_count = await this.numberOfEntries({ contentType })
+    const cbResponse = []
 
     for (let index = 0; index <= entries_count; index += BATCH_SIZE) {
       const entries =
-        (await this.findManyOfCollection({
-          start: index,
+        (await this.getContentTypeEntries({
+          offset: index,
           limit: BATCH_SIZE,
-          collection,
+          contentType,
         })) || []
-      const info = await callback(entries, collection)
-      if (info != null) response.push(info)
+
+      const info = await callback(entries, contentType)
+
+      if (Array.isArray(info)) cbResponse.push(...info)
     }
-    return response
+    return cbResponse
   },
 })
